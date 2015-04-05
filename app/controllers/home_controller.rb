c7edd1c3408a08fce66cd301aaf7d7343b93eb6f
Milly <milly.chuang0624@gmail.com>
2015-04-06 02:04:57 +0800

    Add wheel_time and user CRUD, and gf soultion page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+
+
+  def index
+    default_time = Wheel.first.init_time || Time.now 
+    @start_time = Time.now - (Time.now - default_time).to_i % ONEROUNDTIME
+  end
+
+  def gf_easy_solution
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true
+
+  enum rank: [:user, :admin]
+end
